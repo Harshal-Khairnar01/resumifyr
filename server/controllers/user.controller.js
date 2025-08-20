fbfe802ca8476e7d9fe129cbee2725fd40cff48f
Harshal-Khairnar01 <harshalkhairnar7244@gmail.com>
2025-08-20 08:58:32 +0530

    resume schema done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import user from "../models/user.model";
+import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
@@ -29,7 +29,7 @@ export const register = async (req, res) => {
     const user = await User.create({
       name,
       email,
-      hashedPassword,
+      password: hashedPassword,
     });
 
     res.status(201).json({
